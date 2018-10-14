78ac3fa783c5acbedcc085ce01da447fb544eb16
Miroslav Maksimovic <miki.maksimovic19995@gmail.com>
2018-10-14 13:31:52 +0200

    Request parameters for insert into collection & adding to database.

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
+let db = require('./config/db');
 
 const app = express();
 
 const port = 3000;
 
-require('./app/routes') (app, {});
-app.listen(port, () => {
-    console.log('We arre live at ' + port);
+app.use(bodyParser.urlencoded({extended: true}));
+
+MongoClient.connect(db.url, (err, database) => {
+
+    if (err) return console.log(err);
+
+    db = database.db("notes");
+    require('./app/routes')(app, db);
+
+    app.listen(port, () => {
+        console.log('We are live at ' + port);
+    });
 });
--- app/routes/note-routes.js
+++ app/routes/note-routes.js
@@ -1,5 +1,20 @@
 module.exports = function (app, db) {
 
+    const collection = app.post('/notes', (req, res) => {
+        const note = { text: req.body.body, title: req.body.title};
+
+        db.collection('g_notes').insert(note, (err, result) => {
+
+            if (err) {
+                res.send({'error': 'An error occurred.'})
+            } else {
+                res.send(result.ops[0]);
+            }
+        });
+    });
+
+
+
     app.post('/notes', (req, res) => {
         res.send('Hello World!');
     })
