a1576857232f4a5821361c838660b8189d2d4803
Miroslav Maksimovic <miki.maksimovic19995@gmail.com>
2018-10-15 23:49:17 +0200

    Put request.

--- app/routes/note-routes.js
+++ app/routes/note-routes.js
@@ -30,6 +30,21 @@ module.exports = function (app, db) {
         });
     });
 
+    app.put('/notes/:id', (req, res) => {
+
+        const id = req.params.id;
+        const details = {'_id': new ObjectID(id)};
+        const note = { text: req.body.body, title: req.body.title};
+        db.collection('g_notes').update(details, note,  (err, item) => {
+
+            if (err) {
+                res.send({'error': 'An error has occurred!'});
+            } else {
+                res.send(note);
+            }
+        });
+    });
+
     app.post('/notes', (req, res) => {
         const note = {text: req.body.body, title: req.body.title};
 
