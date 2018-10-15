a53560fd596990bc537eb5df957d6f3ccff5f779
Miroslav Maksimovic <miki.maksimovic19995@gmail.com>
2018-10-15 23:43:52 +0200

    Get route

--- app/routes/note-routes.js
+++ app/routes/note-routes.js
@@ -1,7 +1,23 @@
+const ObjectID = require("mongodb").ObjectID;
+
 module.exports = function (app, db) {
 
-    const collection = app.post('/notes', (req, res) => {
-        const note = { text: req.body.body, title: req.body.title};
+    app.get('/notes/:id', (req, res) => {
+
+        const id = req.params.id;
+        const details = {'_id': new ObjectID(id)};
+        db.collection('g_notes').findOne(details, (err, item) => {
+
+            if (err) {
+                res.send({'error': 'An error has occurred!'});
+            } else {
+                res.send(item);
+            }
+        });
+    });
+
+    app.post('/notes', (req, res) => {
+        const note = {text: req.body.body, title: req.body.title};
 
         db.collection('g_notes').insert(note, (err, result) => {
 
@@ -12,7 +28,6 @@ module.exports = function (app, db) {
             }
         });
     });
-
 
 
     app.post('/notes', (req, res) => {
